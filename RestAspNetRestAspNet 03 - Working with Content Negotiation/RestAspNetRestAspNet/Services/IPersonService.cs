0cff92c5e63063bd157fdac2c3aef136de1bdb0c
DouglasPBergamo <douglas_mi@hotmail.com.br>
2018-12-22 17:11:56 -0200

    implementando camadas ao projeto

---
+++ RestAspNetRestAspNet 03 - Working with Content Negotiation/RestAspNetRestAspNet/Services/IPersonService.cs
@@ -0,0 +1,14 @@
+﻿using RestAspNetRestAspNet.Model;
+using System.Collections.Generic;
+
+namespace RestAspNetRestAspNet.Services
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+        Person FindById(long id);
+        List<Person> FindAll();
+        Person Update(Person person);
+        void Delete(long id);
+    }
+}
