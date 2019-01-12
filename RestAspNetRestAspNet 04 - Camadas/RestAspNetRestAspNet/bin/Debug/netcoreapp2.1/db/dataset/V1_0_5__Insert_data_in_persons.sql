1df39e3bf6f3d6bc347002df4c43c6c4e998e1f9
DouglasPBergamo <douglas_mi@hotmail.com.br>
2019-01-12 17:44:49 -0200

    Generic Repository

---
+++ RestAspNetRestAspNet 04 - Camadas/RestAspNetRestAspNet/bin/Debug/netcoreapp2.1/db/dataset/V1_0_5__Insert_data_in_persons.sql
@@ -0,0 +1,6 @@
+﻿INSERT INTO `persons` (`FirstName`, `LastName`, `Address`, `Gender`) VALUES
+	('Pablo o Falso', 'Escobar', 'Bogotá - Capital - Colômbia', 'Ninguém viu'),
+	('Douglas', 'Bérgamo', 'Jaborandi - São Paulo - Brasil', 'Male'),
+	('Riquelme', 'Júnior', 'Buenos Aires - Buenos Aires - Argentina', 'Male'),
+	('Tamires', 'Juliana', 'Jaborandi - São Paulo - Brasil', 'Male'),
+	('Pablo', 'Escobar', 'Asuncion - Capital - Paraguai', 'NÃO SE SABE');
